c5c90cf4cfe0a9d0004b190317130b8091fce4e7
Ashraf Khan <mohdashrafkkhan@gmail.com>
2026-03-23 09:32:39 +0530

    Added Portfolio Return Sorting (Merge + Quick Sort)

---
+++ src/PortfolioSorting.java
@@ -0,0 +1,116 @@
+import java.util.Random;
+
+class Asset {
+    String name;
+    double returnRate;
+    double volatility;
+
+    Asset(String name, double returnRate, double volatility) {
+        this.name = name;
+        this.returnRate = returnRate;
+        this.volatility = volatility;
+    }
+
+    public String toString() {
+        return name + ":" + returnRate + "%";
+    }
+}
+
+public class PortfolioSorting {
+
+    static void mergeSort(Asset[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+
+    static void merge(Asset[] arr, int l, int m, int r) {
+
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        Asset[] L = new Asset[n1];
+        Asset[] R = new Asset[n2];
+
+        for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i].returnRate <= R[j].returnRate) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    static void quickSort(Asset[] arr, int low, int high) {
+        if (low < high) {
+
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(Asset[] arr, int low, int high) {
+
+        Asset pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            if (arr[j].returnRate > pivot.returnRate ||
+                    (arr[j].returnRate == pivot.returnRate &&
+                            arr[j].volatility < pivot.volatility)) {
+
+                i++;
+
+                Asset temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        Asset temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void printArray(String msg, Asset[] arr) {
+        System.out.println("\n" + msg);
+        for (Asset a : arr) {
+            System.out.println(a);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Asset[] assets = {
+                new Asset("AAPL", 12, 0.3),
+                new Asset("TSLA", 8, 0.6),
+                new Asset("GOOG", 15, 0.2)
+        };
+
+        Asset[] mergeArr = assets.clone();
+        mergeSort(mergeArr, 0, mergeArr.length - 1);
+        printArray("Merge Sort (Ascending Return):", mergeArr);
+
+        Asset[] quickArr = assets.clone();
+        quickSort(quickArr, 0, quickArr.length - 1);
+        printArray("Quick Sort (Desc Return + Asc Volatility):", quickArr);
+    }
+}
